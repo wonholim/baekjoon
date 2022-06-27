1e79129621d2995a8ea58fab3a9870ca83707eb3
bloom_groove <56383948+wonholim@users.noreply.github.com>
2022-06-27 22:30:10 +0900

    [Bronze V] Title: 그대로 출력하기, Time: 240 ms, Memory: 17912 KB -BaekjoonHub

---
+++ 백준/Bronze/11718. 그대로 출력하기/README.md
@@ -0,0 +1,12 @@
+# [Bronze V] 그대로 출력하기 - 11718 
+
+[문제 링크](https://www.acmicpc.net/problem/11718) 
+
+### 성능 요약
+
+메모리: 17912 KB, 시간: 240 ms
+
+### 분류
+
+구현(implementation), 문자열(string)
+
---
+++ 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		String a;
+		while(sc.hasNext()) {
+			a = sc.nextLine();
+			System.out.println(a);
+		}
+
+	}
+
+}
