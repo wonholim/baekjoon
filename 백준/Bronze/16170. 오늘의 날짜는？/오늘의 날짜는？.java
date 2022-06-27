6e243ae3bdc0df288f8cdbc5552a1eec5854a292
bloom_groove <56383948+wonholim@users.noreply.github.com>
2022-06-27 22:34:03 +0900

    [Bronze V] Title: 오늘의 날짜는?, Time: 120 ms, Memory: 13796 KB -BaekjoonHub

---
+++ 백준/Bronze/16170. 오늘의 날짜는？/오늘의 날짜는？.java
@@ -0,0 +1,10 @@
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("2022");
+		System.out.println("06");
+		System.out.println("24");
+	}
+
+}
