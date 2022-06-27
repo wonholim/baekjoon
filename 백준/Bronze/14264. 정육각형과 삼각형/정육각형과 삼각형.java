29bc755738866c05fb18aca8ad5524dcc0864c92
bloom_groove <56383948+wonholim@users.noreply.github.com>
2022-06-28 03:34:12 +0900

    [Bronze IV] Title: 정육각형과 삼각형, Time: 208 ms, Memory: 17700 KB -BaekjoonHub

---
+++ 백준/Bronze/14264. 정육각형과 삼각형/정육각형과 삼각형.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		double s = (((3 * Math.sqrt(3)) / 2) * a * a) / 6;
+		System.out.println(s);
+	}
+}
