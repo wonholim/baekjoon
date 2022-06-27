29d7274ec67a4ac3c6a04e3553cf1d47a9395b81
bloom_groove <56383948+wonholim@users.noreply.github.com>
2022-06-27 22:36:13 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 216 ms, Memory: 17708 KB -BaekjoonHub

---
+++ 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a;
+		Scanner sc = new Scanner(System.in);
+		a = sc.nextInt();
+		System.out.println(a - 543);
+		sc.close();
+	}
+
+}
