49909e0ac0aae4871b5edf092b6a98a9839a1126
bloom_groove <56383948+wonholim@users.noreply.github.com>
2022-06-27 22:19:55 +0900

    [Bronze V] Title: Plane, Time: 204 ms, Memory: 17512 KB -BaekjoonHub

---
+++ 백준/Bronze/8370. Plane/Plane.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		int c = sc.nextInt();
+		int d = sc.nextInt();
+		System.out.println(a * b + c * d);
+	}
+
+}
