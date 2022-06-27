ac3996716d8be068e72e7bf549231323a15bedf1
bloom_groove <56383948+wonholim@users.noreply.github.com>
2022-06-27 22:29:26 +0900

    [Bronze V] Title: 꼬마 정민, Time: 208 ms, Memory: 17800 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/README.md
@@ -0,0 +1,12 @@
+# [Bronze V] 꼬마 정민 - 11382 
+
+[문제 링크](https://www.acmicpc.net/problem/11382) 
+
+### 성능 요약
+
+메모리: 17800 KB, 시간: 208 ms
+
+### 분류
+
+구현(implementation), 사칙연산(arithmetic), 수학(math)
+
---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.java
@@ -0,0 +1,16 @@
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		BigInteger A = sc.nextBigInteger();
+		BigInteger B = sc.nextBigInteger();
+		BigInteger C = sc.nextBigInteger();
+		System.out.println(A.add(B).add(C));
+
+	}
+
+}
