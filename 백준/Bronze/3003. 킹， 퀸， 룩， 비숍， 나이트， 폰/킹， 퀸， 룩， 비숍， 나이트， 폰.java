c0f86c9162558dfbe2909f892c984df5dda513ce
bloom_groove <56383948+wonholim@users.noreply.github.com>
2022-06-27 22:14:52 +0900

    [Bronze V] Title: 킹, 퀸, 룩, 비숍, 나이트, 폰, Time: 284 ms, Memory: 19160 KB -BaekjoonHub

---
+++ 백준/Bronze/3003. 킹， 퀸， 룩， 비숍， 나이트， 폰/킹， 퀸， 룩， 비숍， 나이트， 폰.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		int c = sc.nextInt();
+		int d = sc.nextInt();
+		int e = sc.nextInt();
+		int f = sc.nextInt();
+		System.out.println((1 - a) + " " + (1 - b) + " " + (2 - c) + " " + (2 - d) + " " + (2 - e) + " " + (8 - f) );
+
+		sc.close();
+	}
+}
