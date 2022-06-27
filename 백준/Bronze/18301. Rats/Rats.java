2b214c11d14aa5b276b7786f88c2b8c98bcdd5bb
bloom_groove <56383948+wonholim@users.noreply.github.com>
2022-06-27 22:36:30 +0900

    [Bronze V] Title: Rats, Time: 208 ms, Memory: 17620 KB -BaekjoonHub

---
+++ 백준/Bronze/18301. Rats/Rats.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		int c = sc.nextInt();
+		System.out.print((a + 1)*(b + 1)/(c + 1) -1);
+	}
+
+}
