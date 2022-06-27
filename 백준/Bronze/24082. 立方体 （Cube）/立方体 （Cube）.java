1b41400bbce178f072da8ae1be780708f4390dd2
bloom_groove <56383948+wonholim@users.noreply.github.com>
2022-06-27 22:38:55 +0900

    [Bronze V] Title: 立方体 (Cube), Time: 208 ms, Memory: 17596 KB -BaekjoonHub

---
+++ 백준/Bronze/24082. 立方体 （Cube）/立方体 （Cube）.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		System.out.print(a * a * a);
+	}
+
+}
