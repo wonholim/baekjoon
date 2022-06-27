c1aa379b48eee69280ce14fb017f52e1ec7bb28c
bloom_groove <56383948+wonholim@users.noreply.github.com>
2022-06-27 22:34:51 +0900

    [Bronze V] Title: 달달함이 넘쳐흘러, Time: 252 ms, Memory: 18664 KB -BaekjoonHub

---
+++ 백준/Bronze/17256. 달달함이 넘쳐흘러/달달함이 넘쳐흘러.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		int c = sc.nextInt();
+		int d = sc.nextInt();
+		int e = sc.nextInt();
+		int f = sc.nextInt();
+		System.out.print((d - c) + " " + (e/b) + " " + (f - a));
+	}
+
+}
