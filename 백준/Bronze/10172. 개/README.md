82deb08039be26172f7682ff1a42e813f0cbfae5
bloom_groove <56383948+wonholim@users.noreply.github.com>
2022-06-27 22:24:01 +0900

    [Bronze V] Title: 개, Time: 116 ms, Memory: 13960 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/README.md
@@ -0,0 +1,12 @@
+# [Bronze V] 개 - 10172 
+
+[문제 링크](https://www.acmicpc.net/problem/10172) 
+
+### 성능 요약
+
+메모리: 13960 KB, 시간: 116 ms
+
+### 분류
+
+구현(implementation)
+
---
+++ 백준/Bronze/10172. 개/개.java
@@ -0,0 +1,12 @@
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("|\\_/|");
+		System.out.println("|q p|   /}");
+		System.out.println("( 0 )\"\"\"\\");
+		System.out.println("|\"^\"`    |");
+		System.out.println("||_/=\\\\__|");    
+	}
+
+}
