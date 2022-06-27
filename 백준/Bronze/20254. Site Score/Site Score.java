26f0435e537717fcb1e0068b852b0393bd668862
bloom_groove <56383948+wonholim@users.noreply.github.com>
2022-06-27 22:36:48 +0900

    [Bronze V] Title: Site Score, Time: 212 ms, Memory: 17656 KB -BaekjoonHub

---
+++ 백준/Bronze/20254. Site Score/Site Score.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		int c = sc.nextInt();
+		int d = sc.nextInt();
+		System.out.print(56 * a + 24 * b + 14 * c + 6 * d);
+	}
+
+}
