2166dfe776d1cceaaee4e0a3d64f404c8dcaa0d9
bloom_groove <56383948+wonholim@users.noreply.github.com>
2022-06-27 22:33:49 +0900

    [Bronze V] Title: 이상한 기호, Time: 208 ms, Memory: 17792 KB -BaekjoonHub

---
+++ 백준/Bronze/15964. 이상한 기호/이상한 기호.java
@@ -0,0 +1,15 @@
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		BigInteger a = sc.nextBigInteger();
+		BigInteger b = sc.nextBigInteger();
+		System.out.println(a.add(b).multiply(a.subtract(b)));
+		sc.close();
+	}
+
+}
