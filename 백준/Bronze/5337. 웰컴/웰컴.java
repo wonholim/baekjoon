709206f4bde623c292e6455838c6e406a9b330ad
bloom_groove <56383948+wonholim@users.noreply.github.com>
2022-06-27 22:16:55 +0900

    [Bronze V] Title: 웰컴, Time: 120 ms, Memory: 13880 KB -BaekjoonHub

---
+++ 백준/Bronze/5337. 웰컴/웰컴.java
@@ -0,0 +1,11 @@
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(".  .   .\n"
+				+ "|  | _ | _. _ ._ _  _\n"
+				+ "|/\\|(/.|(_.(_)[ | )(/.");
+
+	}
+
+}
