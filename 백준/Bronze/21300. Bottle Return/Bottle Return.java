083448edc7e48f1a6c43c1096f349c9feedc20db
bloom_groove <56383948+wonholim@users.noreply.github.com>
2022-06-27 22:37:28 +0900

    [Bronze V] Title: Bottle Return, Time: 208 ms, Memory: 17692 KB -BaekjoonHub

---
+++ 백준/Bronze/21300. Bottle Return/Bottle Return.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		int c = sc.nextInt();
+		int d = sc.nextInt();
+		int e = sc.nextInt();
+		int f = sc.nextInt();
+		System.out.println((a * 5) + (b * 5) + (c * 5) + (d * 5) + (e * 5) + (f * 5));
+	}
+
+}
