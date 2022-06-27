88a49b43ed6a3b0f26bb8d740d50bc30d6e87f0c
bloom_groove <56383948+wonholim@users.noreply.github.com>
2022-06-27 22:12:00 +0900

    [Bronze V] Title: N 찍기, Time: 1092 ms, Memory: 33308 KB -BaekjoonHub

---
+++ 백준/Bronze/2741. N 찍기/N 찍기.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		for(int i = 0; i < a; i++) {
+			System.out.println(i+1);
+		}
+	}
+
+}
