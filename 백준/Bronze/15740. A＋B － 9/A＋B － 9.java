d6b40878e01ec0c525075b93503effba9f93d978
bloom_groove <56383948+wonholim@users.noreply.github.com>
2022-06-27 22:33:11 +0900

    [Bronze V] Title: A+B - 9, Time: 420 ms, Memory: 20852 KB -BaekjoonHub

---
+++ 백준/Bronze/15740. A＋B － 9/A＋B － 9.java
@@ -0,0 +1,14 @@
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		BigInteger a = sc.nextBigInteger();
+		BigInteger b = sc.nextBigInteger();
+		System.out.println(a.add(b));
+	}
+
+}
---
+++ 백준/Bronze/15740. A＋B － 9/README.md
@@ -0,0 +1,12 @@
+# [Bronze V] A+B - 9 - 15740 
+
+[문제 링크](https://www.acmicpc.net/problem/15740) 
+
+### 성능 요약
+
+메모리: 20852 KB, 시간: 420 ms
+
+### 분류
+
+임의 정밀도 / 큰 수 연산(arbitrary_precision), 사칙연산(arithmetic), 수학(math)
+
