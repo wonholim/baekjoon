1a005f57a7bcc0b5c44c8c16393af484ae681836
bloom_groove <56383948+wonholim@users.noreply.github.com>
2022-06-27 22:30:43 +0900

    [Bronze V] Title: 큰 수 곱셈, Time: 6180 ms, Memory: 398020 KB -BaekjoonHub

---
+++ 백준/Bronze/13277. 큰 수 곱셈/큰 수 곱셈.java
@@ -0,0 +1,15 @@
+import java.math.BigInteger;
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		BigInteger A = sc.nextBigInteger();
+		BigInteger B = sc.nextBigInteger();
+		System.out.println(A.multiply(B));
+
+	}
+
+}
