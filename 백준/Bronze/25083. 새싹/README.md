f44ecd870fa82c824607cced8fefd60f1e5bd6bc
bloom_groove <56383948+wonholim@users.noreply.github.com>
2022-06-27 22:41:35 +0900

    [Bronze V] Title: 새싹, Time: 124 ms, Memory: 14144 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/README.md
@@ -0,0 +1,12 @@
+# [Bronze V] 새싹 - 25083 
+
+[문제 링크](https://www.acmicpc.net/problem/25083) 
+
+### 성능 요약
+
+메모리: 14144 KB, 시간: 124 ms
+
+### 분류
+
+구현(implementation)
+
---
+++ 백준/Bronze/25083. 새싹/새싹.java
@@ -0,0 +1,11 @@
+public class Main {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	      System.out.printf("         ,%c\'\"%d\n",'r',7);
+	      System.out.printf("%c`-_   ,\'  ,/\n",'r'); 
+	      System.out.printf(" \\. \". %c_%c\'\n",'L','r'); 
+	      System.out.printf("   `~\\/\n");
+	      System.out.printf("      |\n");
+	      System.out.printf("      |\n");
+	}
+}
