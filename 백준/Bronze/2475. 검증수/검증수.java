961c7ce55d8e559ddbf17ce9e9e62d184e68d56d
bloom_groove <56383948+wonholim@users.noreply.github.com>
2022-06-27 22:08:45 +0900

    [Bronze V] Title: 검증수, Time: 204 ms, Memory: 17716 KB -BaekjoonHub

---
+++ 백준/Bronze/2475. 검증수/검증수.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		int c = sc.nextInt();
+		int d = sc.nextInt();
+		int e = sc.nextInt();
+		System.out.println((a * a + b * b + c * c + d * d + e * e) % 10);
+		sc.close();
+	}
+
+}
