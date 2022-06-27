e6fc3f9c79c120efdd5396d4d4238ee8b55f9068
bloom_groove <56383948+wonholim@users.noreply.github.com>
2022-06-27 22:31:14 +0900

    [Bronze V] Title: 와이버스 부릉부릉, Time: 196 ms, Memory: 17400 KB -BaekjoonHub

---
+++ 백준/Bronze/14645. 와이버스 부릉부릉/와이버스 부릉부릉.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		while(sc.hasNext()) {
+		int A = sc.nextInt();
+		int B = sc.nextInt();
+	}
+		System.out.println("비와이");
+	}
+}
