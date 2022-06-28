df2416c008ded6278c93484b24b86eb202473e21
bloom_groove <56383948+wonholim@users.noreply.github.com>
2022-06-29 03:16:39 +0900

    [Bronze IV] Title: CASIO, Time: 204 ms, Memory: 17708 KB -BaekjoonHub

---
+++ 백준/Bronze/15963. CASIO/CASIO.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		long a = sc.nextLong();
+		long b = sc.nextLong();
+		if(a == b) {
+			System.out.println("1");
+		}else {
+			System.out.println("0");
+		}
+
+	}
+
+}
