9415349249e7d4d7b4c2dfa1b6e8defe93e952fc
bloom_groove <56383948+wonholim@users.noreply.github.com>
2022-06-28 23:24:55 +0900

    [Bronze IV] Title: 폰 노이만과 파리, Time: 200 ms, Memory: 17460 KB -BaekjoonHub

---
+++ 백준/Bronze/14924. 폰 노이만과 파리/폰 노이만과 파리.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		int c = sc.nextInt();
+		System.out.println(c / (a * 2) * b);
+	}
+
+}
