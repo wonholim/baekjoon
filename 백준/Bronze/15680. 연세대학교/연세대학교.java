9e0d14c31984ce417351024babb273f72cafd9f6
bloom_groove <56383948+wonholim@users.noreply.github.com>
2022-06-29 02:10:27 +0900

    [Bronze IV] Title: 연세대학교, Time: 200 ms, Memory: 17576 KB -BaekjoonHub

---
+++ 백준/Bronze/15680. 연세대학교/연세대학교.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+
+		if(sc.nextInt() == 0) {
+			System.out.println("YONSEI");
+		}else {
+			System.out.println("Leading the Way to the Future");
+		}
+	}
+
+}
