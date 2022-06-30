58618d7a3a7d8d08afde92f0804549efc6a189fb
bloom_groove <56383948+wonholim@users.noreply.github.com>
2022-06-30 09:05:44 +0900

    [Bronze IV] Title: 치킨댄스를 추는 곰곰이를 본 임스, Time: 212 ms, Memory: 17672 KB -BaekjoonHub

---
+++ 백준/Bronze/25191. 치킨댄스를 추는 곰곰이를 본 임스/치킨댄스를 추는 곰곰이를 본 임스.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		int c = sc.nextInt();
+		b = b / 2;
+		System.out.println(Math.min(a, b + c));
+
+	}
+
+}
