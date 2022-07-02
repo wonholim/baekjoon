a87adde19721610dff4c8e68fb694a6b72aac60c
bloom_groove <56383948+wonholim@users.noreply.github.com>
2022-07-02 17:59:37 +0900

    [Bronze IV] Title: Next in line, Time: 200 ms, Memory: 17644 KB -BaekjoonHub

---
+++ 백준/Bronze/6749. Next in line/Next in line.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+			int A = sc.nextInt();
+			int B = sc.nextInt();
+			System.out.println(B + (B - A));
+	}
+
+}
