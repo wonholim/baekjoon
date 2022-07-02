91de49b5ce0668b3c3b31eb5f6ba6345669f1b9b
bloom_groove <56383948+wonholim@users.noreply.github.com>
2022-07-02 17:56:06 +0900

    [Bronze V] Title: Shares, Time: 208 ms, Memory: 17432 KB -BaekjoonHub

---
+++ 백준/Bronze/3733. Shares/README.md
@@ -0,0 +1,12 @@
+# [Bronze V] Shares - 3733 
+
+[문제 링크](https://www.acmicpc.net/problem/3733) 
+
+### 성능 요약
+
+메모리: 17432 KB, 시간: 208 ms
+
+### 분류
+
+사칙연산(arithmetic), 수학(math)
+
---
+++ 백준/Bronze/3733. Shares/Shares.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		while(sc.hasNext()) {
+			int A = sc.nextInt();
+			int B = sc.nextInt();
+			System.out.println(B / (A + 1));
+		}
+	}
+
+}
