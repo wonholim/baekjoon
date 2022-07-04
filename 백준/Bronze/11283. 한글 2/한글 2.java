c19d54f6e3169203a730a507ecbadd48c19bb31c
bloom_groove <56383948+wonholim@users.noreply.github.com>
2022-07-05 05:47:11 +0900

    [Bronze IV] Title: 한글 2, Time: 196 ms, Memory: 17640 KB -BaekjoonHub

---
+++ 백준/Bronze/11283. 한글 2/한글 2.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+
+public class Main {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		char a = sc.next().charAt(0);
+		System.out.println(a - 44031);
+	}
+}
