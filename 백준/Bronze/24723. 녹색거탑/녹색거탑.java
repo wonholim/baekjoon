7ed20e7d83dddbbc977687dfc9375a54a7ea2671
bloom_groove <56383948+wonholim@users.noreply.github.com>
2022-07-05 06:26:05 +0900

    [Bronze IV] Title: 녹색거탑, Time: 208 ms, Memory: 17600 KB -BaekjoonHub

---
+++ 백준/Bronze/24723. 녹색거탑/녹색거탑.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		System.out.println((int)Math.pow(2, sc.nextInt()));
+	}
+
+}
