1c9cb47335d27df523e7dcdf3180a9ccc97b0c7c
bloom_groove <56383948+wonholim@users.noreply.github.com>
2022-07-05 05:44:22 +0900

    [Bronze IV] Title: 한글, Time: 208 ms, Memory: 17728 KB -BaekjoonHub

---
+++ 백준/Bronze/11282. 한글/한글.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+
+public class Main {
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		System.out.println((char) (a + 44031));
+	}
+}
