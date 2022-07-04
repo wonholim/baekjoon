0c09b5ea662bb8959189b02f166087ca1ef1d094
bloom_groove <56383948+wonholim@users.noreply.github.com>
2022-07-05 05:52:47 +0900

    [Bronze IV] Title: FA, Time: 196 ms, Memory: 17628 KB -BaekjoonHub

---
+++ 백준/Bronze/14935. FA/FA.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+
+public class Main {
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		String a = sc.next();
+		System.out.println("FA");
+	}
+}
