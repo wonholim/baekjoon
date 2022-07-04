bec1c1e1a53fa1be8a03d5cc4bc1d06fca9e029b
bloom_groove <56383948+wonholim@users.noreply.github.com>
2022-07-05 03:58:45 +0900

    [Bronze III] Title: 최소, 최대, Time: 2532 ms, Memory: 337244 KB -BaekjoonHub

---
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.java
@@ -0,0 +1,19 @@
+import java.util.Collections;
+import java.util.Scanner;
+import java.util.Vector;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		Vector<Integer> vc = new Vector<Integer>();
+		for(int i = 0; i < a; i++) {
+			vc.add(sc.nextInt());
+		}
+		Collections.sort(vc);
+		System.out.println(vc.get(0) + " " + vc.get(a-1));
+	}
+
+}
