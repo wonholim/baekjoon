831efef2728af289ff314d085bd0cbcb10b3b583
bloom_groove <56383948+wonholim@users.noreply.github.com>
2022-07-05 04:31:05 +0900

    [Bronze III] Title: 기찍 N, Time: 1084 ms, Memory: 32256 KB -BaekjoonHub

---
+++ 백준/Bronze/2742. 기찍 N/기찍 N.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		for(int i = a; 0 < i; i--) {
+			System.out.println(i);
+		}
+	}
+
+}
