2b94c19f4ec67f71923e372abd54ee9e7eb22acd
bloom_groove <56383948+wonholim@users.noreply.github.com>
2022-07-09 22:42:39 +0900

    [Bronze III] Title: 직각삼각형, Time: 212 ms, Memory: 17648 KB -BaekjoonHub

---
+++ 백준/Bronze/4153. 직각삼각형/직각삼각형.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc= new Scanner(System.in);
+		while(true) {
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		int c = sc.nextInt();
+		if(a == 0 && b == 0 && c == 0 ) {
+			break;
+		}
+			if(a * a + b * b == c * c || b * b + c * c == a * a || a * a + c * c == b * b) {
+				System.out.println("right");
+			}else {
+				System.out.println("wrong");
+			}
+		}
+	}
+
+}
