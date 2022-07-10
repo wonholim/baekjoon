2487e8d93c19563a660cafd842d1f57616ee25e3
bloom_groove <56383948+wonholim@users.noreply.github.com>
2022-07-10 14:52:55 +0900

    [Bronze III] Title: 줄 세기, Time: 204 ms, Memory: 17432 KB -BaekjoonHub

---
+++ 백준/Bronze/4806. 줄 세기/줄 세기.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc= new Scanner(System.in);
+		int a = 0;
+		while(sc.hasNext()) {
+			String b = sc.nextLine();
+			a++;
+		}
+		System.out.println(a);
+	}
+
+}
