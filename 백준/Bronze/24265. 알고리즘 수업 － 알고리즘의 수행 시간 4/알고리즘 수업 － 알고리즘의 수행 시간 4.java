c0defdf94af985ac76aaf553da4d960d79c16bb0
bloom_groove <56383948+wonholim@users.noreply.github.com>
2022-07-14 22:35:48 +0900

    [Bronze III] Title: 알고리즘 수업 - 알고리즘의 수행 시간 4, Time: 216 ms, Memory: 17660 KB -BaekjoonHub

---
+++ 백준/Bronze/24265. 알고리즘 수업 － 알고리즘의 수행 시간 4/알고리즘 수업 － 알고리즘의 수행 시간 4.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		Long a = sc.nextLong();
+		System.out.println((a * (a - 1)) / 2);
+		System.out.println("2");
+	}
+
+}
