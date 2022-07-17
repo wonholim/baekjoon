66e28839e08dd7a87153248118cc0f1cd69ab5b5
bloom_groove <56383948+wonholim@users.noreply.github.com>
2022-07-18 01:49:32 +0900

    [Bronze III] Title: 별 찍기 - 17, Time: 436 ms, Memory: 23352 KB -BaekjoonHub

---
+++ 백준/Bronze/10992. 별 찍기 － 17/별 찍기 － 17.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc = new Scanner(System.in);
+		int a = sc.nextInt();
+		for(int i = 0; i < a; i++) {
+			for(int j = a - 1; i < j; j--){
+				System.out.print(" ");
+			}
+			for(int k = 0; k < 1; k++) {
+				System.out.print("*");
+			}
+			for(int j = 0; j < 2 * i - 1; j++){
+				if(i != a - 1) {
+				System.out.print(" ");
+				}else {
+					System.out.print("*");
+				}
+			}
+			if(i != 0) {
+				System.out.print("*");
+			}
+			System.out.println("");
+		}
+	}
+
+}
