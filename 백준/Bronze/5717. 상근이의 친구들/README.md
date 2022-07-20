9a3b42c75f67df210eab15d1a88b0d588b0a7273
bloom_groove <56383948+wonholim@users.noreply.github.com>
2022-07-20 22:18:44 +0900

    [Bronze III] Title: 상근이의 친구들, Time: 208 ms, Memory: 17616 KB -BaekjoonHub

---
+++ 백준/Bronze/5717. 상근이의 친구들/README.md
@@ -0,0 +1,12 @@
+# [Bronze III] 상근이의 친구들 - 5717 
+
+[문제 링크](https://www.acmicpc.net/problem/5717) 
+
+### 성능 요약
+
+메모리: 17616 KB, 시간: 208 ms
+
+### 분류
+
+사칙연산(arithmetic), 수학(math)
+
---
+++ 백준/Bronze/5717. 상근이의 친구들/상근이의 친구들.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc= new Scanner(System.in);
+		while(true) {
+		int a = sc.nextInt();
+		int b = sc.nextInt();
+		if(a == 0 && b == 0) {
+			break;
+		}
+		System.out.println(a + b);
+		}
+	}
+
+}
