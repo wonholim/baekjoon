d232ced337a96756833129f85c4ca071f64e15d3
bloom_groove <56383948+wonholim@users.noreply.github.com>
2022-07-22 22:53:49 +0900

    [Bronze II] Title: 그대로 출력하기 2, Time: 232 ms, Memory: 17960 KB -BaekjoonHub

---
+++ 백준/Bronze/11719. 그대로 출력하기 2/그대로 출력하기 2.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner sc= new Scanner(System.in);
+		while(sc.hasNext()) {
+			String a = sc.nextLine();
+			System.out.println(a);
+		}
+	}
+}
