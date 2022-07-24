fa5265ea8beeedaae84114b697e08d507c54fb18
bloom_groove <56383948+wonholim@users.noreply.github.com>
2022-07-25 02:55:06 +0900

    [Silver V] Title: 최소공배수, Time: 132 ms, Memory: 14356 KB -BaekjoonHub

---
+++ 백준/Silver/13241. 최소공배수/최소공배수.java
@@ -0,0 +1,29 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		long a = Long.parseLong(st.nextToken());
+		long b = Long.parseLong(st.nextToken());
+		bw.write(Long.toString((a * b) / gcd(a, b) ));
+		bw.flush();
+		bw.close();
+		br.close();
+	}
+
+	private static long gcd(long a, long b) {
+		// TODO Auto-generated method stub
+		if(b == 0) return a;
+		return gcd(b, a % b);
+	}
+
+}
