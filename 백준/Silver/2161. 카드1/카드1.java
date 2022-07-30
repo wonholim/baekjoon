aa1c4694d077e082af31a61ea91c9738087bc480
bloom_groove <56383948+wonholim@users.noreply.github.com>
2022-07-30 11:09:00 +0900

    [Silver V] Title: 카드1, Time: 128 ms, Memory: 14444 KB -BaekjoonHub

---
+++ 백준/Silver/2161. 카드1/카드1.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		StringBuilder sb = new StringBuilder();
+		int a = Integer.parseInt(st.nextToken());
+		Queue<Integer> q = new LinkedList<>();
+		for(int i = 1; i <= a; i++) {
+			q.offer(i);
+		}
+		while(!q.isEmpty()) {
+			if(q.size() == 1) {
+				sb.append(q.poll());
+				break;
+			}
+			sb.append(q.poll()).append(" ");
+			int b = q.poll();
+			q.offer(b);
+		}
+		bw.write(sb.toString());
+		bw.flush();
+		bw.close();
+		br.close();
+	}
+
+}
