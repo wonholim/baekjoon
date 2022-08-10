19ec22dd373b3fe84bf0bc15a386838b2f1d2e67
bloom_groove <56383948+wonholim@users.noreply.github.com>
2022-08-11 05:00:47 +0900

    [Silver III] Title: 2×n 타일링 2, Time: 128 ms, Memory: 14360 KB -BaekjoonHub

---
+++ 백준/Silver/11727. 2×n 타일링 2/2×n 타일링 2.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+	static int[] b;
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		// TODO Auto-generated method stub
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringBuilder sb = new StringBuilder();
+		int a = Integer.parseInt(br.readLine());
+		b = new int[1001];
+		sb.append(dp(a));
+		bw.write(sb.toString());	
+		bw.flush();
+		bw.close();
+		br.close();
+	}
+	private static int dp(int a) {
+		// TODO Auto-generated method stub
+		if(a == 1) return 1;
+		if(a == 2) return 3;
+		if(b[a] > 0) return b[a];
+		return b[a] = (dp(a - 1) + 2 * dp(a - 2)) % 10007;
+	}
+
+
+}
