e4be7bd2e2ca71b163d8d921db76de2ea9eba11e
bloom_groove <56383948+wonholim@users.noreply.github.com>
2022-08-12 02:48:40 +0900

    [Silver V] Title: 피보나치 수 4, Time: 188 ms, Memory: 25380 KB -BaekjoonHub

---
+++ 백준/Silver/10826. 피보나치 수 4/피보나치 수 4.java
@@ -0,0 +1,32 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.math.BigInteger;
+
+public class Main {
+
+
+	static BigInteger[] b;
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringBuilder sb = new StringBuilder();
+		int a = Integer.parseInt(br.readLine());
+		b = new BigInteger[1000001];
+		b[0] = BigInteger.ZERO;
+		b[1] = BigInteger.ONE;
+		b[2] = BigInteger.ONE;
+		for(int i = 3; i <= a; i++) {
+			b[i] = b[i - 1].add(b[i - 2]);
+		}
+		sb.append(b[a]);
+		bw.write(sb.toString());
+		bw.flush();
+		bw.close();
+		br.close();
+	}
+
+}
