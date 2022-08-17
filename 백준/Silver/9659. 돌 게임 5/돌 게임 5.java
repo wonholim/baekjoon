d84314aea708c02fcbfdf3db13a508f792d40e89
bloom_groove <56383948+wonholim@users.noreply.github.com>
2022-08-17 23:14:24 +0900

    [Silver III] Title: 돌 게임 5, Time: 124 ms, Memory: 14232 KB -BaekjoonHub

---
+++ 백준/Silver/9659. 돌 게임 5/돌 게임 5.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringBuilder sb = new StringBuilder();
+		long a = Long.parseLong(br.readLine());
+		if(a % 2 != 0) {
+			sb.append("SK");
+		}else {
+			sb.append("CY");
+		}
+		bw.write(sb.toString());
+		bw.flush();
+		bw.close();
+		br.close();
+	}
+
+}
