91adff27c2e2203fc6e6d95bd883b4830b6b022f
bloom_groove <56383948+wonholim@users.noreply.github.com>
2022-09-04 20:40:25 +0900

    [Bronze III] Title: J박스, Time: 124 ms, Memory: 14080 KB -BaekjoonHub

---
+++ 백준/Bronze/5354. J박스/J박스.java
@@ -0,0 +1,46 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st;
+		int a = Integer.parseInt(br.readLine());
+		while(a --> 0) {
+			int b = Integer.parseInt(br.readLine());
+			if(b == 1) {
+				sb.append("#").append("\n").append("\n");
+				continue;
+			}
+			int c = b - 2;
+			for(int i = 0; i < b; i++) {
+				sb.append("#");
+			}
+			sb.append("\n");
+			while(c --> 0) {
+				sb.append("#");
+				for(int i = 0; i < b - 2; i++) {
+					sb.append("J");
+				}
+				sb.append("#").append("\n");
+			}
+			for(int i = 0; i < b; i++) {
+				sb.append("#");
+			}
+			sb.append("\n").append("\n");
+		}
+		bw.write(sb.toString());
+		bw.flush();
+		bw.close();
+		br.close();
+	}
+
+}
