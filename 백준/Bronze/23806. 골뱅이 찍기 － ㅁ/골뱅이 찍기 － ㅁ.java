c56abf07d28d79789d0fd77710cee9739aeda5c9
bloom_groove <56383948+wonholim@users.noreply.github.com>
2022-09-09 21:30:40 +0900

    [Bronze III] Title: 골뱅이 찍기 - ㅁ, Time: 188 ms, Memory: 17420 KB -BaekjoonHub

---
+++ 백준/Bronze/23806. 골뱅이 찍기 － ㅁ/골뱅이 찍기 － ㅁ.java
@@ -0,0 +1,46 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringBuilder sb = new StringBuilder();
+		int a = Integer.parseInt(br.readLine());
+		int b = a * 3;
+		for(int j = 0; j < a; j++) {
+			for(int i = 0; i < 5 * a; i++) {
+				sb.append("@");
+			}
+			sb.append("\n");
+		}
+		for(int i = 0; i < b; i++) {
+			for(int j = 0; j < a; j++) {
+				sb.append("@");
+			}
+			for(int k = 0; k < 3 * a; k++) {
+				sb.append(" ");
+			}
+			for(int j = 0; j < a; j++) {
+				sb.append("@");
+			}
+			sb.append("\n");
+		}
+		for(int j = 0; j < a; j++) {
+			for(int i = 0; i < 5 * a; i++) {
+				sb.append("@");
+			}
+			sb.append("\n");
+		}
+		bw.write(sb.toString());
+		bw.flush();
+		bw.close();
+		br.close();
+	}
+
+}
