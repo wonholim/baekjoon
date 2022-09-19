55b37c08fe99bd455cbefbc8db272f6647999231
bloom_groove <56383948+wonholim@users.noreply.github.com>
2022-09-19 16:33:30 +0900

    [Bronze II] Title: 카이사르 암호, Time: 120 ms, Memory: 14244 KB -BaekjoonHub

---
+++ 백준/Bronze/5598. 카이사르 암호/카이사르 암호.java
@@ -0,0 +1,28 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringBuilder sb = new StringBuilder();
+		String a = br.readLine();
+		for(char c : a.toCharArray()) {
+			if(c - 3 < 65) {
+				c = (char) (c + 26);
+			}
+			sb.append((char)(c - 3));
+		}
+		bw.write(sb.toString());
+		bw.flush();
+		bw.close();
+		br.close();
+	}
+
+}
