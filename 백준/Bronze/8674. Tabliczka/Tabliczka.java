24f0803d79574f8ccfac9dbc669fef9cd50a4a16
bloom_groove <56383948+wonholim@users.noreply.github.com>
2022-09-22 16:02:54 +0900

    [Bronze IV] Title: Tabliczka, Time: 128 ms, Memory: 14212 KB -BaekjoonHub

---
+++ 백준/Bronze/8674. Tabliczka/Tabliczka.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		long a = Long.parseLong(st.nextToken());
+		long b = Long.parseLong(st.nextToken());
+		if(a % 2 == 0 || b % 2 == 0) sb.append(0);
+		else sb.append(Math.min(a, b));
+		bw.write(sb.toString());
+		bw.flush();
+		bw.close();
+		br.close();
+	}
+
+}
