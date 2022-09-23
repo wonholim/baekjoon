ecc0206081948c67ed589cd26b7b1a7b9258a238
bloom_groove <56383948+wonholim@users.noreply.github.com>
2022-09-23 18:35:18 +0900

    [Silver II] Title: 알고리즘 수업 - 너비 우선 탐색 4, Time: 1020 ms, Memory: 87140 KB -BaekjoonHub

---
+++ 백준/Silver/24447. 알고리즘 수업 － 너비 우선 탐색 4/알고리즘 수업 － 너비 우선 탐색 4.java
@@ -0,0 +1,81 @@
+    import java.io.BufferedReader;
+	import java.io.BufferedWriter;
+	import java.io.IOException;
+	import java.io.InputStreamReader;
+	import java.io.OutputStreamWriter;
+	import java.util.ArrayDeque;
+	import java.util.ArrayList;
+	import java.util.Collections;
+	import java.util.Queue;
+	import java.util.StringTokenizer;
+
+	public class Main {
+		static boolean[] check;
+		static ArrayList<Integer>[] link;
+		static long[] ce;
+		static long[] se;
+		static int count = 1;
+		public static void main(String[] args) throws IOException {
+			// TODO Auto-generated method stub
+			BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+			BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+			StringBuilder sb = new StringBuilder();
+			StringTokenizer st = new StringTokenizer(br.readLine());
+			int b = Integer.parseInt(st.nextToken());
+			int c = Integer.parseInt(st.nextToken());
+			int g = Integer.parseInt(st.nextToken());
+			link = new ArrayList[b + 1];
+			check = new boolean[b + 1];
+			ce = new long[b + 1];
+			se = new long[b + 1];
+			for(int i = 1; i <= b; i++) {
+				link[i] = new ArrayList<>();
+				ce[i] = -1;
+			}
+			for(int i = 0; i < c; i++) {
+				st = new StringTokenizer(br.readLine());
+				int d = Integer.parseInt(st.nextToken());
+				int e = Integer.parseInt(st.nextToken());
+				link[d].add(e);
+				link[e].add(d);
+			}
+			for(int i = 1; i <= b; i++) {
+				Collections.sort(link[i]);
+			}
+			bfs(g, 0);
+			long sum = 0;
+			for(int i = 1; i <= b; i++) {
+				sum += se[i] * ce[i];
+			}
+			sb.append(sum);
+			bw.write(sb.toString());
+			bw.flush();
+			bw.close();
+			br.close();
+		}
+		private static void bfs(int i, int g) {
+			// TODO Auto-generated method stub
+			Queue<Integer> q = new ArrayDeque<>();
+			q.offer(i);
+			ce[i] = g;
+			se[i] = count++;
+			check[i] = true;
+			g++;
+			while(!q.isEmpty()) {
+				int size = q.size();
+				while(size --> 0) {
+					int get = q.poll();
+					for(int k : link[get]) {
+						if(!check[k]) {
+							check[k] = true;
+							ce[k] = g;
+							se[k] = count++;
+							q.offer(k);
+						}
+					}
+				}
+				g++;
+			}
+		}
+
+	}
