95f948b4a7518499d4cd01403b6ab8f384cf388b
bloom_groove <56383948+wonholim@users.noreply.github.com>
2022-09-24 18:37:10 +0900

    [Bronze III] Title: 369, Time: 288 ms, Memory: 76860 KB -BaekjoonHub

---
+++ 백준/Bronze/17614. 369/369.java
@@ -0,0 +1,33 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st;
+		StringBuilder sb = new StringBuilder();
+		int a = Integer.parseInt(br.readLine());
+		int count = 0;
+		for(int i = 1; i <= a; i++) {
+			for(char c : Integer.toString(i).toCharArray()) {
+				if(c == '3' || c == '6' || c == '9') {
+					count++;
+				}
+			}
+		}
+		sb.append(count);
+		bw.write(sb.toString());
+		bw.flush();
+		bw.close();
+		br.close();
+	}
+
+}
