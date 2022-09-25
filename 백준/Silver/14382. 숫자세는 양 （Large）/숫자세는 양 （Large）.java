49935a064dd247c676634fa4be52b3d3c4061763
bloom_groove <56383948+wonholim@users.noreply.github.com>
2022-09-25 10:42:22 +0900

    [Silver IV] Title: 숫자세는 양 (Large), Time: 172 ms, Memory: 16412 KB -BaekjoonHub

---
+++ 백준/Silver/14382. 숫자세는 양 （Large）/숫자세는 양 （Large）.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st;
+		StringBuilder sb = new StringBuilder();
+		int a = Integer.parseInt(br.readLine());
+		int i = 1;
+		while(a --> 0) {
+			Set<Character> s = new HashSet<>();
+			long b = Long.parseLong(br.readLine());
+			if(b == 0) {sb.append("Case #" + i + ": INSOMNIA\n"); i++; continue;}
+			int j = 1;
+			while(s.size() != 10) {
+				for(char c : Long.toString(b * j).toCharArray()) {
+					s.add(c);
+				}
+				j++;
+			}
+			sb.append("Case #" + i + ": " + b * (j - 1) + "\n");
+			i++;
+		}
+		bw.write(sb.toString());
+		bw.flush();
+		bw.close();
+		br.close();
+	}
+
+}
