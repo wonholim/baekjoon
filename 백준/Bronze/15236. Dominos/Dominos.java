dd22f44beb805b70c5289a9c7a3769ff7c27ff82
bloom_groove <56383948+wonholim@users.noreply.github.com>
2022-10-04 11:56:50 +0900

    [Bronze III] Title: Dominos, Time: 120 ms, Memory: 14152 KB -BaekjoonHub

---
+++ 백준/Bronze/15236. Dominos/Dominos.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringBuilder sb = new StringBuilder();
+		int a = Integer.parseInt(br.readLine());
+		sb.append(a * (a + 1) * (a + 2) / 2);
+		bw.write(sb.toString());
+		bw.flush();
+		bw.close();
+		br.close();
+	}
+
+}
