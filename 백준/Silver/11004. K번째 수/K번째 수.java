71af31a4cd647164db31f6a28a43eec12fded68f
bloom_groove <56383948+wonholim@users.noreply.github.com>
2022-10-05 15:17:21 +0900

    [Silver V] Title: K번째 수, Time: 2296 ms, Memory: 379248 KB -BaekjoonHub

---
+++ 백준/Silver/11004. K번째 수/K번째 수.java
@@ -0,0 +1,33 @@
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int a = Integer.parseInt(st.nextToken());
+		int b = Integer.parseInt(st.nextToken());
+		int[] c = new int[a];
+		st = new StringTokenizer(br.readLine());
+		for(int i = 0; i < a; i++) {
+			c[i] = Integer.parseInt(st.nextToken());
+		}
+		Arrays.sort(c);
+		sb.append(c[b - 1]);
+		bw.write(sb.toString());
+		bw.flush();
+		bw.close();
+		br.close();
+	}
+
+}
