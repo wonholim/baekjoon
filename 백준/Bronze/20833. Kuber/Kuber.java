60ae0a28ae750095c173a085ecac71f4025dd879
bloom_groove <56383948+wonholim@users.noreply.github.com>
2022-10-17 14:44:10 +0900

    [Bronze IV] Title: Kuber, Time: 124 ms, Memory: 14212 KB -BaekjoonHub

---
+++ 백준/Bronze/20833. Kuber/Kuber.java
@@ -0,0 +1,30 @@
+
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		StringBuilder sb = new StringBuilder();
+		int a = Integer.parseInt(st.nextToken());
+		long sum = 0;
+		for(int i = 1; i <= a; i++) {
+			sum += i * i * i;
+		}
+		sb.append(sum);
+		bw.write(sb.toString());
+		bw.flush();
+		bw.close();
+		br.close();
+	}
+
+}
