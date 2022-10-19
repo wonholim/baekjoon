e7eee4617bec0b4bc0ad749de4a27d355aa132e8
bloom_groove <56383948+wonholim@users.noreply.github.com>
2022-10-19 15:18:01 +0900

    [Bronze IV] Title: Atrium, Time: 132 ms, Memory: 14372 KB -BaekjoonHub

---
+++ 백준/Bronze/20353. Atrium/Atrium.java
@@ -0,0 +1,27 @@
+
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		double a = Double.parseDouble(st.nextToken());
+		sb.append(Math.sqrt(a) * 4);
+		bw.write(sb.toString());
+		bw.flush();
+		bw.close();
+		br.close();
+	}
+
+}
