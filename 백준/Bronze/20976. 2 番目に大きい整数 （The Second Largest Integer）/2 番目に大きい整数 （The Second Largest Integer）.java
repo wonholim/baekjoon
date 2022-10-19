342f3a09d3d97655e5f5e3a1c014144baa6f36f3
bloom_groove <56383948+wonholim@users.noreply.github.com>
2022-10-19 15:15:30 +0900

    [Bronze IV] Title: 2 番目に大きい整数 (The Second Largest Integer), Time: 120 ms, Memory: 14284 KB -BaekjoonHub

---
+++ 백준/Bronze/20976. 2 番目に大きい整数 （The Second Largest Integer）/2 番目に大きい整数 （The Second Largest Integer）.java
@@ -0,0 +1,30 @@
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int[] a = new int[3];
+		for(int i = 0; i < 3; i++) {
+			a[i] = Integer.parseInt(st.nextToken());
+		}
+		Arrays.sort(a);
+		sb.append(a[1]);
+		bw.write(sb.toString());
+		bw.flush();
+		bw.close();
+		br.close();
+	}
+
+}
