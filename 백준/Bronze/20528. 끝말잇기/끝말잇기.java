9e15df0b2c200778df989e332ef477fdb06274be
bloom_groove <56383948+wonholim@users.noreply.github.com>
2022-10-27 14:51:23 +0900

    [Bronze II] Title: 끝말잇기, Time: 124 ms, Memory: 14264 KB -BaekjoonHub

---
+++ 백준/Bronze/20528. 끝말잇기/끝말잇기.java
@@ -0,0 +1,38 @@
+
+
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.HashSet;
+import java.util.Scanner;
+import java.util.Set;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringBuilder sb = new StringBuilder();
+		int a = Integer.parseInt(br.readLine());
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		Set<Character> s = new HashSet<>();
+		while(a --> 0) {
+			String b = st.nextToken();
+			s.add(b.charAt(0));
+			s.add(b.charAt(b.length() - 1));
+		}
+		if(s.size() == 1) sb.append(1);
+		else sb.append(0);
+		bw.write(sb.toString());
+		bw.flush();
+		bw.close();
+		br.close();
+	}
+}
+
+
