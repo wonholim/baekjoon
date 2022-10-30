c7011600c355f20064a0628d550786a208dc9e21
bloom_groove <56383948+wonholim@users.noreply.github.com>
2022-10-30 19:23:13 +0900

    [Bronze III] Title: 골뱅이 찍기 - 뒤집힌 ㅋ, Time: 164 ms, Memory: 15436 KB -BaekjoonHub

---
+++ 백준/Bronze/23810. 골뱅이 찍기 － 뒤집힌 ㅋ/골뱅이 찍기 － 뒤집힌 ㅋ.java
@@ -0,0 +1,47 @@
+
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st;
+		StringBuilder sb = new StringBuilder();
+		int a = Integer.parseInt(br.readLine());
+		for(int i = 0; i < a; i++) {
+			for(int k = 0; k < a * 5; k++) {
+				sb.append("@");
+			}
+			sb.append("\n");
+		}	
+		for(int j = 0; j < a; j++) {
+				for(int k = 0; k < a; k++) {
+					sb.append("@");
+				}
+				sb.append("\n");
+			}
+		for(int i = 0; i < a; i++) {
+			for(int k = 0; k < a * 5; k++) {
+				sb.append("@");
+			}
+			sb.append("\n");
+		}	
+		for(int j = 0; j < a * 2; j++) {
+			for(int k = 0; k < a; k++) {
+				sb.append("@");
+			}
+			sb.append("\n");
+		}
+		bw.write(sb.toString());
+		bw.flush();
+		bw.close();
+		br.close();
+	}
+}
