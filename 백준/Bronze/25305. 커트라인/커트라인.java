58d4503feaa78a8f6739d84cc510e0df74026c9f
bloom_groove <56383948+wonholim@users.noreply.github.com>
2022-10-31 11:58:03 +0900

    [Bronze II] Title: 커트라인, Time: 140 ms, Memory: 14444 KB -BaekjoonHub

---
+++ 백준/Bronze/25305. 커트라인/커트라인.java
@@ -0,0 +1,38 @@
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int a = Integer.parseInt(st.nextToken());
+		int b = Integer.parseInt(st.nextToken());
+		Integer[] c = new Integer[a];
+		st = new StringTokenizer(br.readLine());
+		for(int i = 0; i < a; i++) {
+			c[i] = Integer.parseInt(st.nextToken());
+		}
+		Arrays.sort(c, Collections.reverseOrder());
+		sb.append(c[b - 1]);
+		bw.write(sb.toString());	
+		bw.flush();
+		bw.close();
+		br.close();
+	}
+
+}
