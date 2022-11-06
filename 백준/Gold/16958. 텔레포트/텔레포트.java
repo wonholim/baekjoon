64d0ed6c18eb907a3e917b56894efe9c8a66cf82
bloom_groove <56383948+wonholim@users.noreply.github.com>
2022-11-06 22:53:34 +0900

    [Gold IV] Title: 텔레포트, Time: 228 ms, Memory: 17300 KB -BaekjoonHub

--- 백준/Gold/16958. 텔레포트/텔레포트.java
+++ 백준/Gold/16958. 텔레포트/텔레포트.java
@@ -5,14 +5,14 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.StringTokenizer;
-// 플로이드 워셜
+// 기하학
 public class Main {
 	static int t;
-	static int[][] c;
-	static int[][] cc;
-	static int INF = 100000;
+	static int[][] total_nodes;
+	static ArrayList<int[]> teleport_nodes;
+	static int[] node_values;
+	static int INF = 10000;
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -21,50 +21,53 @@ public class Main {
 		StringBuilder sb = new StringBuilder();
 		int n = Integer.parseInt(st.nextToken());
 		t = Integer.parseInt(st.nextToken());
-		c = new int[n + 1][3];
-		cc = new int[n + 1][n + 1];
-		for(int i = 1; i < n + 1; i++) {
-			for(int j = 1; j < n + 1; j++) {
-				if(i != j) cc[i][j] = INF;
-			}
-		}
+		total_nodes = new int[n + 1][2];
+		teleport_nodes = new ArrayList<>();
+		node_values = new int[n + 1];
 		for(int i = 1; i < n + 1; i++) {
 			st = new StringTokenizer(br.readLine());
-			c[i][0] = Integer.parseInt(st.nextToken());
-			c[i][1] = Integer.parseInt(st.nextToken());
-			c[i][2] = Integer.parseInt(st.nextToken());
+			int s = Integer.parseInt(st.nextToken());
+			int x = Integer.parseInt(st.nextToken());
+			int y = Integer.parseInt(st.nextToken());
+			total_nodes[i][0] = x;
+			total_nodes[i][1] = y;
+			if(s == 1) {
+				teleport_nodes.add(new int[] {x, y});
+			}
 		}
 		for(int i = 1; i < n + 1; i++) {
-			for(int j = 1; j < n + 1; j++) {
-				if(i != j) {
-					int g = Math.abs(c[i][1] - c[j][1]) + Math.abs(c[i][2] - c[j][2]);
-					if(c[i][0] == 1 && c[j][0] == 1) {
-						if(g > t) {
-							g = t;
-						}
-					}
-					cc[i][j] = cc[j][i] = g;
+			int x1 = total_nodes[i][0];
+			int y1 = total_nodes[i][1];
+			int node_value = INF;
+			for(int[] teleportable_point : teleport_nodes) {
+				int x2 = teleportable_point[0];
+				int y2 = teleportable_point[1];
+				int distance = dist(x1, y1, x2, y2);
+				if(distance < node_value) {
+					node_value = distance;
 				}
 			}
-		}
-		for(int k = 1; k < n + 1; k++) {
-			for(int i = 1; i < n + 1; i++) {
-				for(int j = 1; j < n + 1; j++) {
-					cc[i][j] = Math.min(cc[i][j], cc[i][k] + cc[k][j]);
-				}
-			}
+			node_values[i] = node_value;
 		}
 		int a = Integer.parseInt(br.readLine());
 		while(a --> 0) {
 			st = new StringTokenizer(br.readLine());
-			int p = Integer.parseInt(st.nextToken());
-			int o = Integer.parseInt(st.nextToken());
-			sb.append(cc[p][o] + "\n");
+			int start = Integer.parseInt(st.nextToken());
+			int end = Integer.parseInt(st.nextToken());
+			int x1 = total_nodes[start][0];
+			int y1 = total_nodes[start][1];
+			int x2 = total_nodes[end][0];
+			int y2 = total_nodes[end][1];
+			sb.append(Math.min(dist(x1,  y1, x2, y2), node_values[start] + t + node_values[end]) + "\n");
 		}
 		bw.write(sb.toString());
 		bw.flush();
 		bw.close();
 		br.close();
+	}
+	private static int dist(int x1, int y1, int x2, int y2) {
+		// TODO Auto-generated method stub
+		return Math.abs(x1 - x2) + Math.abs(y1 - y2);
 	}
 
 }
