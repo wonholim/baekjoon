74d0cb615dcac74f519b67e881479acf5668aa17
bloom_groove <56383948+wonholim@users.noreply.github.com>
2022-11-07 12:36:34 +0900

    [Bronze III] Title: Practice: Roll Call, Time: 192 ms, Memory: 16780 KB -BaekjoonHub

---
+++ 백준/Bronze/9306. Practice： Roll Call/Practice： Roll Call.java
@@ -0,0 +1,29 @@
+
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringBuilder sb = new StringBuilder();
+		int a = Integer.parseInt(br.readLine());
+		for(int i = 0; i < a; i++) {
+			String b = br.readLine();
+			String c = br.readLine();
+			sb.append("Case " + (i + 1) + ": " + c + ", " + b + "\n");
+		}
+		bw.write(sb.toString());
+		bw.flush();
+		bw.close();
+		br.close();
+	}
+
+}
