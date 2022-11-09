832dd7dfb6965ebf048375a48a4892e2aebb47ef
bloom_groove <56383948+wonholim@users.noreply.github.com>
2022-11-09 14:44:26 +0900

    [Gold V] Title: チーズ (Cheese), Time: 652 ms, Memory: 111552 KB -BaekjoonHub

---
+++ 백준/Gold/5558. チーズ （Cheese）/チーズ （Cheese）.java
@@ -0,0 +1,81 @@
+
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayDeque;
+import java.util.Queue;
+import java.util.StringTokenizer;
+
+public class Main {
+	static int a;
+	static int b;
+	static int t;
+	static int[] dx = {1, -1, 0, 0};
+	static int[] dy = {0, 0, 1, -1};
+	static boolean[][] d;
+	static int[][] c;
+	static Queue<int[]> q;
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		a = Integer.parseInt(st.nextToken());
+		b = Integer.parseInt(st.nextToken());
+		t = Integer.parseInt(st.nextToken());
+		c = new int[a][b];
+		d = new boolean[a][b];
+		q = new ArrayDeque<>();
+		for(int i = 0; i < a; i++) {
+			String g = br.readLine();
+			for(int j = 0; j < b; j++) {
+				if(g.charAt(j) == 'S') {
+					d[i][j] = true;
+					q.offer(new int[] {i, j, 0});
+				}else if(g.charAt(j) == 'X') {
+					c[i][j] = -1;
+				}else if(g.charAt(j) == '.'){
+					c[i][j] = 0;
+				}else{
+					c[i][j] = g.charAt(j) - '0';
+				}
+			}
+		}
+		int count = 0;
+		int level = 1;
+		while(level <= t) {
+			while(!q.isEmpty()) {
+				int[] g = q.poll();
+				if(c[g[0]][g[1]] == level) {
+					count += g[2];
+					q.clear();
+					q.offer(new int[] {g[0], g[1], 0});
+					d = new boolean[a][b];
+					d[g[0]][g[1]] = true;
+					break;
+				}
+				for(int k = 0; k < 4; k++) {
+					int i = g[0] + dx[k];
+					int j = g[1] + dy[k];
+					if(i >= 0 && j >= 0 && i < a && j < b) {
+						if(!d[i][j] && c[i][j] != -1) {
+							d[i][j] = true;
+							q.offer(new int[] {i, j, g[2] + 1});
+						}
+					}
+				}
+			}
+			level++;
+		}
+		sb.append(count);
+		bw.write(sb.toString());
+		bw.flush();
+		bw.close();
+		br.close();
+	}
+
+}
