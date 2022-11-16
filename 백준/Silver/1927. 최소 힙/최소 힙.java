e8daa8f1fef444643b99dab01672c8be15f49e57
Vardy <56383948+wonholim@users.noreply.github.com>
2022-11-16 10:44:21 +0900

    [Silver II] Title: 최소 힙, Time: 364 ms, Memory: 26888 KB -BaekjoonHub

---
+++ 백준/Silver/1927. 최소 힙/최소 힙.java
@@ -0,0 +1,40 @@
+
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+
+public class Main {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringBuilder sb = new StringBuilder();
+		int t = Integer.parseInt(br.readLine());
+		PriorityQueue<Integer> pq = new PriorityQueue<>();
+		while(t --> 0) {
+			int k = Integer.parseInt(br.readLine());
+			if(k == 0) {
+				if(!pq.isEmpty()) {
+					sb.append(pq.poll() + "\n");
+				}else {
+					sb.append(0 + "\n");
+				}
+			}else {
+				pq.offer(k);
+			}
+		}
+		bw.write(sb.toString());	
+		bw.flush();
+		bw.close();
+		br.close();
+	}
+
+}
