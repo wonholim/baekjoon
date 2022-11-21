7ef999a5a1b1843067ca91e39a73623808fea3fe
Vardy <56383948+wonholim@users.noreply.github.com>
2022-11-21 14:39:46 +0900

    [Silver III] Title: N과 M (4), Time: 212 ms, Memory: 20648 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.java
@@ -0,0 +1,43 @@
+
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+	static int[] c;
+	static boolean[] v;
+	static StringBuilder sb;
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		sb = new StringBuilder();
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int n = Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(st.nextToken());
+		c = new int[m];
+		backTracking(m, n, 0, 1);
+		bw.write(sb.toString());	
+		bw.flush();
+		bw.close();
+		br.close();
+	}
+	private static void backTracking(int m, int n, int i, int start) {
+		// TODO Auto-generated method stub
+		if(m == i) {
+			for(int k : c) {
+				sb.append(k + " ");
+			}
+			sb.append("\n");
+			return;
+		}
+		for(int j = start; j <= n; j++) {
+			c[i] = j;
+			backTracking(m, n, i + 1, j);
+		}
+	}
+}
