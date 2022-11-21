87388cb37011985903f5370507b18c18feecae6c
Vardy <56383948+wonholim@users.noreply.github.com>
2022-11-21 14:45:21 +0900

    [Silver II] Title: N과 M (12), Time: 248 ms, Memory: 22164 KB -BaekjoonHub

---
+++ 백준/Silver/15666. N과 M （12）/N과 M （12）.java
@@ -0,0 +1,57 @@
+
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.LinkedHashSet;
+import java.util.StringTokenizer;
+
+public class Main {
+	static int[] c;
+	static boolean[] v;
+	static StringBuilder sb;
+	static ArrayList<Integer> list = new ArrayList<>();
+	static LinkedHashSet<String> s = new LinkedHashSet<>();
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		sb = new StringBuilder();
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int n = Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(st.nextToken());
+		c = new int[m];
+		st = new StringTokenizer(br.readLine());
+		for(int i = 0; i < n; i++) {
+			list.add(Integer.parseInt(st.nextToken()));
+		}
+		Collections.sort(list);
+		backTracking(m, n, 0, 1);
+		for(String g : s) {
+			sb.append(g + "\n");
+		}
+		bw.write(sb.toString());	
+		bw.flush();
+		bw.close();
+		br.close();
+	}
+	private static void backTracking(int m, int n, int i, int start) {
+		// TODO Auto-generated method stub
+		if(m == i) {
+			String k = "";
+			for(int j = 0; j < m; j++) {
+				k += c[j] + " ";
+			}
+			s.add(k);
+			return;
+		}
+		for(int j = start; j <= n; j++) {
+			c[i] = list.get(j - 1);
+			backTracking(m, n, i + 1, j);
+		}
+	}
+}
