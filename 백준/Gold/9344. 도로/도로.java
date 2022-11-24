5de9243b6c79df3daaf4a96b28d016d5018f91b1
Vardy <56383948+wonholim@users.noreply.github.com>
2022-11-24 17:40:00 +0900

    [Gold III] Title: 도로, Time: 364 ms, Memory: 26628 KB -BaekjoonHub

---
+++ 백준/Gold/9344. 도로/도로.java
@@ -0,0 +1,103 @@
+
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+
+
+public class Main {
+	public static class Node implements Comparable<Node> {
+		int t;
+		int f;
+		int d;
+
+		public Node(int t, int f, int d) {
+			this.t = t;
+			this.f = f;
+			this.d = d;
+		}
+
+		public int compareTo(Node o) {
+			return this.d - o.d;
+		}
+	}
+
+	static int n;
+	static int[] parent;
+	static PriorityQueue<Node> MST;
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st;
+		int time = Integer.parseInt(br.readLine());
+		while(time --> 0) {
+			st = new StringTokenizer(br.readLine());
+			n = Integer.parseInt(st.nextToken());
+			int t = Integer.parseInt(st.nextToken());
+			String gogogo = st.nextToken() + " " + st.nextToken();
+			parent = new int[n + 1];
+			for (int i = 1; i < n + 1; i++) {
+				parent[i] = i;
+			}
+			MST = new PriorityQueue<>();
+			long max = 0;
+			while (t-- > 0) {
+				st = new StringTokenizer(br.readLine());
+				int x = Integer.parseInt(st.nextToken());
+				int y = Integer.parseInt(st.nextToken());
+				int d = Integer.parseInt(st.nextToken());
+				MST.add(new Node(x, y, d));
+				max += d;
+			}
+			int t2 = MST.size();
+			int count = 1;
+			boolean check = false;
+			while (t2-- > 0) {
+				Node cur = MST.poll();
+				if (!find(cur.t, cur.f)) {
+					count++;
+					unionParent(cur.t, cur.f);
+					if(gogogo.equals(cur.t + " " + cur.f)) {
+						check = true;
+					}
+				}
+			}
+			if(count == n && check) sb.append("YES\n");
+			else sb.append("NO\n");
+		}
+		bw.write(sb.toString());
+		bw.flush();
+		bw.close();
+		br.close();
+	}
+	private static void unionParent(int x, int y) {
+		// TODO Auto-generated method stub
+		x = getParent(x);
+		y = getParent(y);
+		if (x > y)
+			parent[y] = x;
+		else
+			parent[x] = y;
+	}
+
+	private static boolean find(int x, int y) {
+		// TODO Auto-generated method stub
+		x = getParent(x);
+		y = getParent(y);
+		return x == y ? true : false;
+	}
+
+	private static int getParent(int x) {
+		// TODO Auto-generated method stub
+		if (parent[x] == x)
+			return x;
+		return parent[x] = getParent(parent[x]);
+	}
+}
