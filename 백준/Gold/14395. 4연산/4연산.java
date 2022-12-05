55709cfd8f38ee68cab7653a9cd57412be8aada3
Vardy <56383948+wonholim@users.noreply.github.com>
2022-12-05 19:41:33 +0900

    [Gold V] Title: 4연산, Time: 136 ms, Memory: 14436 KB -BaekjoonHub

--- 백준/Gold/14395. 4연산/4연산.java
+++ 백준/Gold/14395. 4연산/4연산.java
@@ -7,8 +7,10 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 import java.util.ArrayDeque;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
 import java.util.Queue;
+import java.util.Set;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -22,6 +24,7 @@ public class Main {
 	}
 	static boolean[] d;
 	static Queue<Node> q;
+	static Set<Long> s = new HashSet<>();
 	public static void main(String[] args) throws IOException {
 		// TODO Auto-generated method stub
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -31,26 +34,25 @@ public class Main {
 		long a = Long.parseLong(st.nextToken());
 		long b = Long.parseLong(st.nextToken());
 		if(a == b) {System.out.println(0); return;}
-		d = new boolean[(int) b + 1];
 		q = new ArrayDeque<>();
 		q.offer(new Node("", a));
 		while(!q.isEmpty()) {
 			Node n = q.poll();
 			if(n.g == b) {System.out.println(n.out); return;}
-			if(n.g * n.g <= b && !d[(int) (n.g * n.g)]) {
-				d[(int) (n.g * n.g)] = true;
+			if(n.g * n.g <= b && !s.contains(n.g * n.g)) {
+				s.add(n.g * n.g);
 				q.offer(new Node(n.out + "*", n.g * n.g));
 			}
-			if(n.g + n.g <= b && !d[(int) (n.g + n.g)]) {
-				d[(int) (n.g + n.g)] = true;
+			if(n.g + n.g <= b && !s.contains(n.g + n.g)) {
+				s.add(n.g + n.g);
 				q.offer(new Node(n.out + "+", n.g + n.g));
 			}
-			if(n.g - n.g <= b && !d[(int) (n.g - n.g)]) {
-				d[(int) (n.g - n.g)] = true;
+			if(n.g - n.g <= b && !s.contains(n.g - n.g)) {
+				s.add(n.g - n.g);
 				q.offer(new Node(n.out + "-", n.g - n.g));
 			}
-			if(n.g != 0 && n.g / n.g <= b && !d[(int) (n.g / n.g)]) {
-				d[(int) (n.g / n.g)] = true;
+			if(n.g != 0 && n.g / n.g <= b && !s.contains(n.g / n.g)) {
+				s.add(n.g / n.g);
 				q.offer(new Node(n.out + "/", n.g / n.g));
 			}
 		}
