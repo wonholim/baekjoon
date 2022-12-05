a247ed1ad380343b15103cbf38bdf6f99247174e
Vardy <56383948+wonholim@users.noreply.github.com>
2022-12-05 20:58:49 +0900

    [Bronze II] Title: 진짜 공간, Time: 128 ms, Memory: 14288 KB -BaekjoonHub

---
+++ 백준/Bronze/1350. 진짜 공간/진짜 공간.java
@@ -0,0 +1,45 @@
+
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st;
+		int t = Integer.parseInt(br.readLine());
+		int[] c = new int[t];
+		st = new StringTokenizer(br.readLine());
+		for(int i = 0 ; i < t; i++) {
+			c[i] = Integer.parseInt(st.nextToken());
+		}
+		int k = Integer.parseInt(br.readLine());
+		long sum = 0;
+		for(int i = 0; i < t; i++) {
+			if(c[i] == 0) continue;
+			if(c[i] > k) {
+				if(c[i] % k == 0) {
+					sum += (c[i] / k) * k;
+				}else {
+					sum += ((c[i] / k) + 1) * k;
+				}
+			}else {
+				sum += k;
+			}
+		}
+		sb.append(sum);
+		bw.write(sb.toString());	
+		bw.flush();
+		bw.close();
+		br.close();
+	}
+
+}
