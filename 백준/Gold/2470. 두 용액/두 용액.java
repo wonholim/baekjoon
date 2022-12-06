387820bda8138c84d3e9fd732a483449511ccb82
Vardy <56383948+wonholim@users.noreply.github.com>
2022-12-06 21:26:42 +0900

    [Gold V] Title: 두 용액, Time: 400 ms, Memory: 28564 KB -BaekjoonHub

--- 백준/Gold/2470. 두 용액/두 용액.java
+++ 백준/Gold/2470. 두 용액/두 용액.java
@@ -1,4 +1,5 @@
 
+
 import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.IOException;
@@ -15,51 +16,36 @@ public class Main {
 		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 		StringBuilder sb = new StringBuilder();
 		StringTokenizer st = new StringTokenizer(br.readLine());
-		int m = Integer.parseInt(st.nextToken());
-		int[] a = new int[m + 1];
+		int n = Integer.parseInt(st.nextToken());
+		int[] c = new int[n];
 		st = new StringTokenizer(br.readLine());
-		for(int i = 1; i <= m; i++) {
-			a[i] = Integer.parseInt(st.nextToken());
+		for(int i = 0; i < n; i++) {
+			c[i] = Integer.parseInt(st.nextToken());
 		}
-		Arrays.sort(a, 1, m + 1);
-		int best_sum = Integer.MAX_VALUE;
-		int v1 = 0;
-		int v2 = 0;
-		for(int i = 1; i <= m - 1; i++) {
-			int result = lower_bound(a, i + 1, m, -a[i]);
-			
-			if(i + 1 <= result - 1 && result - 1 <= m && Math.abs(a[result - 1] + a[i]) < best_sum) {
-				best_sum = Math.abs(a[result - 1] + a[i]);
-				v1 = a[i];
-				v2 = a[result - 1];
+		Arrays.sort(c);
+		int min = c[0];
+		int max = c[n - 1];
+		int tmp = Math.abs(max + min);
+		int l = 0;
+		int r = n - 1;
+		while(l < r) {
+			int sum = c[r] + c[l];
+			if(Math.abs(sum) < tmp) {
+				tmp = Math.abs(sum);
+				min = c[l];
+				max = c[r];
 			}
-			
-			if(i + 1 <= result && result <= m && Math.abs(a[result] + a[i]) < best_sum) {
-				best_sum = Math.abs(a[result] + a[i]);
-				v1 = a[i];
-				v2 = a[result];
+			if(sum > 0) {
+				r--;
+			}else {
+				l++;
 			}
 		}
-		sb.append(v1 + " " + v2);
-		bw.write(sb.toString());
+		sb.append(min + " " + max);
+		bw.write(sb.toString());	
 		bw.flush();
 		bw.close();
 		br.close();
-	}
-
-	private static int lower_bound(int[] a, int l, int r, int s) {
-		// TODO Auto-generated method stub
-		int result = r + 1;
-		while(l <= r) {
-			int mid = (l + r) / 2;
-			if(a[mid] >= s) {
-				result = mid;
-				r = mid - 1;
-			}else {
-				l = mid + 1;
-			}
-		}
-		return result;
 	}
 
 }
