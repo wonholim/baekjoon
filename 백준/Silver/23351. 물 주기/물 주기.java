13b9d89fe5fc020bfa6fe76cb32c7e6d707b94eb
Vardy <56383948+wonholim@users.noreply.github.com>
2022-12-12 20:02:17 +0900

    [Silver III] Title: 물 주기, Time: 128 ms, Memory: 15084 KB -BaekjoonHub

---
+++ 백준/Silver/23351. 물 주기/물 주기.java
@@ -0,0 +1,47 @@
+
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int n = Integer.parseInt(st.nextToken());
+		int k = Integer.parseInt(st.nextToken());
+		int a = Integer.parseInt(st.nextToken());
+		int b = Integer.parseInt(st.nextToken());
+		int[] c = new int[n];
+		for(int i = 0; i < n; i++) {
+			c[i] = k;
+		}
+		int count = 0;
+		while(true) {
+			if(c[0] == 0) break;
+			count++;
+			for(int i = 0; i < a; i++) {
+				c[i] += b;
+			}
+			for(int i = 0; i < n; i++) {
+				c[i]--;
+			}
+			Arrays.sort(c);
+		}
+		sb.append(count);
+		bw.write(sb.toString());	
+		bw.flush();
+		bw.close();
+		br.close();
+	}
+
+}
