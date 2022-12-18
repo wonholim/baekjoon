612618acdbc109bafa94a5346c6b3ca628e1a7db
Vardy <56383948+wonholim@users.noreply.github.com>
2022-12-18 21:29:54 +0900

    [Bronze V] Title: Helicopter, Time: 64 ms, Memory: 5324 KB -BaekjoonHub

---
+++ 백준/Bronze/14337. Helicopter/Helicopter.vb
@@ -0,0 +1,11 @@
+   Module HelloWorld
+      Sub Main( )
+         System.Console.WriteLine("      ===================")
+         System.Console.WriteLine("          ____||___")
+         System.Console.WriteLine("\ /      /       []\")
+         System.Console.WriteLine(" X=======           \__")
+         System.Console.WriteLine("/ \      \____________|")
+         System.Console.WriteLine("            ||  ||")
+         System.Console.WriteLine("         \-----------/")
+      End Sub
+   End Module
