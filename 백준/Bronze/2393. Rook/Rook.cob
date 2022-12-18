cfd95fbe298a8d4ac63e8d68e0e55ecd9d5ddecc
Vardy <56383948+wonholim@users.noreply.github.com>
2022-12-18 21:27:37 +0900

    [Bronze V] Title: Rook, Time: 8 ms, Memory: 70348 KB -BaekjoonHub

---
+++ 백준/Bronze/2393. Rook/Rook.cob
@@ -0,0 +1,18 @@
+000100  IDENTIFICATION DIVISION. 
+000200  PROGRAM-ID. MFPROG1.     
+000300  PROCEDURE DIVISION.      
+000400  MAIN-PARA.               
+000500      DISPLAY '  ___  ___  ___'
+000600      DISPLAY '  | |__| |__| |'
+000700      DISPLAY '  |           |'
+000800      DISPLAY '   \_________/'
+000900      DISPLAY '    \_______/'
+001000      DISPLAY '     |     |'
+001100      DISPLAY '     |     |'
+001200      DISPLAY '     |     |'
+001300      DISPLAY '     |     |'
+001400      DISPLAY '     |_____|'
+001500      DISPLAY '  __/       \__'
+001600      DISPLAY ' /             \'
+001700      DISPLAY '/_______________\'
+001800      STOP RUN.
