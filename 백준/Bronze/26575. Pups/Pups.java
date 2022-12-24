82a6b488eda8513a4324ae432aea15d15cc069e0
Vardy <56383948+wonholim@users.noreply.github.com>
2022-12-25 02:02:04 +0900

    [Bronze V] Title: Pups, Time: 144 ms, Memory: 16140 KB -BaekjoonHub

---
+++ 백준/Bronze/26575. Pups/Pups.java
@@ -0,0 +1,32 @@
+
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		StringBuilder sb = new StringBuilder();
+		int t = Integer.parseInt(st.nextToken());
+		while(t --> 0) {
+			st = new StringTokenizer(br.readLine());
+			double a = Double.parseDouble(st.nextToken());
+			double b = Double.parseDouble(st.nextToken());
+			double c = Double.parseDouble(st.nextToken());
+			sb.append("$"+String.format("%.2f", a * b * c) + "\n");
+		}
+		bw.write(sb.toString());
+		bw.flush();
+		bw.close();
+		br.close();
+	}
+
+}
