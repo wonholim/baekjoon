5087af3c0902bd3c2c1e867d291dbbb1b407204a
Vardy <56383948+wonholim@users.noreply.github.com>
2022-12-24 21:32:39 +0900

    [Bronze V] Title: Serca, Time: 316 ms, Memory: 54368 KB -BaekjoonHub

---
+++ 백준/Bronze/26766. Serca/Serca.java
@@ -0,0 +1,36 @@
+
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringTokenizer st;
+		StringBuilder sb = new StringBuilder();
+		int t = Integer.parseInt(br.readLine());
+		while(t --> 0) {
+			sb.append(" @@@   @@@ \n"
+					+ "@   @ @   @\n"
+					+ "@    @    @\n"
+					+ "@         @\n"
+					+ " @       @ \n"
+					+ "  @     @  \n"
+					+ "   @   @   \n"
+					+ "    @ @    \n"
+					+ "     @     \n");
+		}
+		bw.write(sb.toString());
+		bw.flush();
+		bw.close();
+		br.close();
+	}
+
+}
