b9edc1b9e203bd65122a4c9f8db435ca3fbdb5ec
Vardy <56383948+wonholim@users.noreply.github.com>
2022-12-26 01:06:32 +0900

    [Silver V] Title: 세 부분, Time: 256 ms, Memory: 25052 KB -BaekjoonHub

---
+++ 백준/Silver/2993. 세 부분/세 부분.java
@@ -0,0 +1,49 @@
+
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		String line = st.nextToken();
+		StringBuilder sbd;
+		ArrayList<String> list = new ArrayList<>();
+		for(int i = 0; i < line.length() - 2; i++) {
+			for(int j = i + 1; j < line.length() - 1; j++) {
+				for(int k = j + 1; k < line.length(); k++) {
+					sbd = new StringBuilder();
+					String one = line.substring(i, j);
+					one = sbd.append(one).reverse().toString();
+					sbd.setLength(0);
+					String two = line.substring(j, k);
+					two = sbd.append(two).reverse().toString();
+					sbd.setLength(0);
+					String three = line.substring(k, line.length());
+					three = sbd.append(three).reverse().toString();
+					sbd.setLength(0);
+					if((one.length() + two.length() + three.length()) == line.length()) list.add(one + two + three);
+				}
+			}
+		}
+		Collections.sort(list);
+		sb.append(list.get(0));
+		bw.write(sb.toString());
+		bw.flush();
+		bw.close();
+		br.close();
+	}
+
+}
