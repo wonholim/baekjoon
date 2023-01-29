a1247d9b19489d82d12901977fa1f2380437625b
Vardy <56383948+wonholim@users.noreply.github.com>
2023-01-30 06:02:01 +0900

    [Bronze V] Title: 長方形 (Rectangle), Time: 128 ms, Memory: 14196 KB -BaekjoonHub

---
+++ 백준/Bronze/27323. 長方形 （Rectangle）/長方形 （Rectangle）.java
@@ -0,0 +1,26 @@
+
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Collections;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st;
+		sb.append(Integer.parseInt(br.readLine()) * Integer.parseInt(br.readLine()));
+		bw.write(sb.toString());	
+		bw.flush();
+		bw.close();
+		br.close();
+	}
+
+}
