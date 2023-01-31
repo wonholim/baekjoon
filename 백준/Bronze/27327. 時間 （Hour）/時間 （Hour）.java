a0c4ade478f8b4d3fb20ac3d746eff72e3917922
Vardy <56383948+wonholim@users.noreply.github.com>
2023-01-31 19:15:15 +0900

    [Bronze V] Title: 時間 (Hour), Time: 220 ms, Memory: 17576 KB -BaekjoonHub

---
+++ 백준/Bronze/27327. 時間 （Hour）/時間 （Hour）.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.*;
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+        Scanner sc = new Scanner(System.in);
+        int i = sc.nextInt();
+		System.out.print(i * 24);
+	}
+
+}
