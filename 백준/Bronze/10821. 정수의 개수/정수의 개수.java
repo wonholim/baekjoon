fcb235a9efa94315429417fbc590e94c2ec756a4
Vardy <56383948+wonholim@users.noreply.github.com>
2023-02-04 21:59:57 +0900

    [Bronze II] Title: 정수의 개수, Time: 124 ms, Memory: 14200 KB -BaekjoonHub

---
+++ 백준/Bronze/10821. 정수의 개수/정수의 개수.java
@@ -0,0 +1,26 @@
+
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st;
+		String[] k = br.readLine().split(",");
+		sb.append(k.length);
+		bw.write(sb.toString());	
+		bw.flush();
+		bw.close();
+		br.close();
+	}
+
+}
