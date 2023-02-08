58344734ae5f5f9160d97114d976e47dd79946a0
Vardy <56383948+wonholim@users.noreply.github.com>
2023-02-08 20:36:28 +0900

    [Silver V] Title: Q-인덱스, Time: 132 ms, Memory: 14528 KB -BaekjoonHub

---
+++ 백준/Silver/13333. Q－인덱스/Q－인덱스.java
@@ -0,0 +1,40 @@
+
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int n = Integer.parseInt(st.nextToken());
+		st = new StringTokenizer(br.readLine());
+		int[] c = new int[n];
+		for(int i = 0; i < n; i++) {
+			c[i] = Integer.parseInt(st.nextToken());
+		}
+		Arrays.sort(c);
+		for(int i = 0; i < n; i++) {
+			int q = n - i;
+			if(c[i] >= q) {
+				System.out.println(q);
+				return;
+			}
+		}
+		sb.append(0);
+		bw.write(sb.toString());
+		bw.flush();
+		bw.close();
+		br.close();
+	}
+
+}
