2741d7eb318679dfcd418983731e4891830e6d54
Vardy <56383948+wonholim@users.noreply.github.com>
2023-02-10 11:43:50 +0900

    [Silver IV] Title: 꿀 아르바이트, Time: 312 ms, Memory: 26148 KB -BaekjoonHub

---
+++ 백준/Silver/12847. 꿀 아르바이트/꿀 아르바이트.java
@@ -0,0 +1,42 @@
+
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int n = Integer.parseInt(st.nextToken());
+		int m = Integer.parseInt(st.nextToken());
+		long[] c = new long[n];
+		long[] pre = new long[n];
+		st = new StringTokenizer(br.readLine());
+		for(int i = 0; i < n; i++) {
+			c[i] = Integer.parseInt(st.nextToken());
+		}
+		pre[0] = c[0];
+		for(int i = 1; i < n; i++) {
+			pre[i] = pre[i - 1] + c[i];
+		}
+		long max = pre[m - 1];
+		int k = 0;
+		for(int i = m; i < n; i++) {
+			max = Math.max(max, pre[i] - pre[k++]);
+		}
+		sb.append(max);
+		bw.write(sb.toString());
+		bw.flush();
+		bw.close();
+		br.close();
+	}
+
+}
