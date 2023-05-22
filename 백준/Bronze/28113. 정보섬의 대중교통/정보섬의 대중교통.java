ee55c4467b812bd5982ece58a72a25cb67f6f24a
Vardy <56383948+wonholim@users.noreply.github.com>
2023-05-22 09:36:06 +0900

    [Bronze V] Title: 정보섬의 대중교통, Time: 124 ms, Memory: 14172 KB -BaekjoonHub

---
+++ 백준/Bronze/28113. 정보섬의 대중교통/정보섬의 대중교통.java
@@ -0,0 +1,32 @@
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int a = Integer.parseInt(st.nextToken());
+		int b = Integer.parseInt(st.nextToken());
+		int c = Integer.parseInt(st.nextToken());
+		if(a == c && c == b) sb.append("Anything");
+		else {
+			if(c == b) sb.append("Anything");
+			else sb.append(c > b ? "Bus" : "Subway");
+		}
+		
+		bw.write(sb.toString());
+		bw.flush();
+		bw.close();
+		br.close();
+	}
+}
