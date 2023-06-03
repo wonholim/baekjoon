475a3068f2738ed040a79618a1390d94b8848c41
Vardy <56383948+wonholim@users.noreply.github.com>
2023-06-03 22:26:47 +0900

    [Bronze IV] Title: Skarpetki, Time: 268 ms, Memory: 22040 KB -BaekjoonHub

---
+++ 백준/Bronze/26742. Skarpetki/Skarpetki.java
@@ -0,0 +1,33 @@
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st;
+		String l = br.readLine();
+		int a = 0;
+		for (char c : l.toCharArray()) {
+			if(c == 'C') a++;
+		}
+		sb.append((a / 2) + ((l.length() - a )/ 2));
+		bw.write(sb.toString());
+		bw.flush();
+		bw.close();
+		br.close();
+	}
+
+}
