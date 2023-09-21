2bdbcd2f802021654c1a6b35629e32455fba9c5d
Vardy <56383948+wonholim@users.noreply.github.com>
2023-09-21 15:42:40 +0900

    [Bronze V] Title: 진흥원 세미나, Time: 40 ms, Memory: 31256 KB -BaekjoonHub

---
+++ 백준/Bronze/30087. 진흥원 세미나/진흥원 세미나.py
@@ -0,0 +1,12 @@
+a = {
+    "Algorithm": "204",
+    "DataAnalysis": "207",
+    "ArtificialIntelligence": "302",
+    "CyberSecurity": "B101",
+    "Network": "303",
+    "Startup": "501",
+    "TestStrategy": "105"
+}
+b = int(input())
+for i in range(b):
+    print(a.get(input()))
