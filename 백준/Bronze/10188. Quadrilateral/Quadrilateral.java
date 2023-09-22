9e5ae0d6dd889ad745247bdbf60d53b77426ff7c
Vardy <56383948+wonholim@users.noreply.github.com>
2023-09-23 00:48:18 +0900

    [Bronze IV] Title: Quadrilateral, Time: 120 ms, Memory: 14004 KB -BaekjoonHub

---
+++ 백준/Bronze/10188. Quadrilateral/Quadrilateral.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+public class Main {
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st;
+		int n = Integer.parseInt(br.readLine());
+		while (n --> 0) {
+			st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			while (b --> 0) {
+				for (int i = 0; i < a; i++) {
+					sb.append("X");
+				}
+				sb.append('\n');
+			}
+			sb.append("\n");
+		}
+		System.out.println(sb.toString());
+	}
+}
---
+++ 백준/Bronze/10188. Quadrilateral/README.md
@@ -0,0 +1,24 @@
+# [Bronze IV] Quadrilateral - 10188 
+
+[문제 링크](https://www.acmicpc.net/problem/10188) 
+
+### 성능 요약
+
+메모리: 14004 KB, 시간: 120 ms
+
+### 분류
+
+구현
+
+### 문제 설명
+
+<p>A quadrilateral is a 4-sided figure. For this program, print out a 4 sided figure using the capital letter X. The largest side will be 20, and the smallest side will be 1.</p>
+
+### 입력 
+
+ <p>The first line consists of the number of data sets. Each data set consists of 2 integers, the length and width of the quadrilateral.</p>
+
+### 출력 
+
+ <p>Print out the quadrilateral using the capital letter X, each quadrilateral separated by a blank line.</p>
+
