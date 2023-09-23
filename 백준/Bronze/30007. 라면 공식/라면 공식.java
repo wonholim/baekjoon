9101cd8c95addc88c7ce560b9d3e6783697d364c
Vardy <56383948+wonholim@users.noreply.github.com>
2023-09-23 23:14:49 +0900

    [Bronze V] Title: 라면 공식, Time: 128 ms, Memory: 14228 KB -BaekjoonHub

---
+++ 백준/Bronze/30007. 라면 공식/라면 공식.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+public class Main {
+	public static void main(String[] args) throws IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringBuilder sb = new StringBuilder();
+		StringTokenizer st;
+		int n = Integer.parseInt(br.readLine());
+		while (n --> 0) {
+			st = new StringTokenizer(br.readLine());
+			int a = Integer.parseInt(st.nextToken());
+			int b = Integer.parseInt(st.nextToken());
+			int x = Integer.parseInt(st.nextToken());
+			sb.append((a * (x - 1)) + b).append('\n');
+		}
+		System.out.println(sb.toString());
+	}
+}
