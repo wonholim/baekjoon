bafc781666fe1dc8ccdecad8264ca29e1b9565d5
Vardy <kds0034@gmail.com>
2023-12-26 23:36:31 +0900

    [Gold V] Title: 개똥벌레, Time: 588 ms, Memory: 49192 KB -BaekjoonHub

---
+++ 백준/Gold/3020. 개똥벌레/개똥벌레.java
@@ -0,0 +1,36 @@
+import java.io.*;
+import java.util.*;
+public class Main {
+	public static void main(String[] args) throws NumberFormatException, IOException {
+		// TODO Auto-generated method stub
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int n = Integer.parseInt(st.nextToken());
+		int h = Integer.parseInt(st.nextToken());
+		int[] topPrefix = new int[h + 1];
+		int[] bottomPrefix = new int[h + 1];
+		for (int i = 0; i < n; i++){
+			int k = Integer.parseInt(br.readLine());
+			if (i % 2 == 0) bottomPrefix[k]++;
+			else topPrefix[k]++;
+		}
+		List<Integer> list = new ArrayList<>();
+		for (int i = h - 1; i > 0; i--) {
+			topPrefix[i] += topPrefix[i + 1];
+			bottomPrefix[i] += bottomPrefix[i + 1];
+		}
+		for (int i = 1; i <= h; i++) {
+			list.add(topPrefix[i] + bottomPrefix[h - i + 1]);
+		}
+		
+		Collections.sort(list);
+		
+		System.out.println(
+				list.get(0)
+				+ " " 
+				+ list.stream()
+					.takeWhile(x -> list.get(0).equals(x))
+					.count()
+		);
+	}
+}
